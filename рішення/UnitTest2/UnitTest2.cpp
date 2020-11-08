47e722c95bb6319c401015a92da6487ac76979fa
KlikSen <ppsha27012003@gmail.com>
2020-11-08 19:25:28 +0300

    еуіе

---
+++ рішення/UnitTest2/UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5_1/lab5_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = h(0, 2);
+			Assert::AreEqual(t, 3);
+		}
+	};
+}
